05378604d7e2c1f9a12326e85f22a87d5aa6d7b3
Samir Vimercati <vimercati.samir@gmail.com>
2024-09-24 19:05:48 +0200

    feat: first javascript lesson

---
+++ javascript_lezione_1/index.js
@@ -0,0 +1,100 @@
+/* let numberOne = 2;
+let numberTwo = 10;
+
+// alert(numberOne + numberTwo)
+
+numberOne = 4.2;
+
+// alert(numberOne + numberTwo)
+
+const pi = 3.14;
+
+const username = "cicciogamer";
+
+const listOfItems = ["latte", "cioccolata", "pane"];
+
+const person = {
+  name: "ciccio",
+  surname: "paguro",
+  hobby: "fortnite",
+};
+*/
+
+/* const numberOne = 0;
+const numberTwo = 10;
+
+const boolean = false;
+
+console.log(numberOne + numberTwo);
+console.log(numberOne - numberTwo);
+console.log(numberOne * numberTwo);
+console.log(numberOne / numberTwo); */
+
+/*if (numberOne == numberTwo) {
+  console.log("è uguale");
+} */
+
+/* if (numberOne === boolean) {
+  console.log("si è uguale sono falsi");
+}
+
+const weather = "sunning"; */
+
+/* if (weather === "sunning") {
+  console.log("lascio l'ombrello a casa");
+} else {
+  console.log("porto l'ombrello");
+} */
+
+/* if (weather === "sunning") {
+  console.log("porto crema solare");
+} else if (weather === "raining") {
+  console.log("porto ombrello");
+} else {
+  console.log("esco normalmente");
+}
+
+if (numberOne > numberTwo) {
+  console.log("numero 1 è maggiore di numero 2");
+} else {
+  console.log("numero 2 è maggiore di numero 1");
+}
+
+const questionOne = Number(prompt("Quanti anni hai?"));
+
+console.log(questionOne); */
+
+/* for (let index = 0; index < 10; index++) {
+  console.log(index);
+} */
+
+/* let index = 0;
+
+while (index < 10) {
+  console.log(index);
+  index++;
+} */
+
+const shoppingList = [
+  "pane",
+  "acqua",
+  "latte",
+  "cioccolata",
+  "verdure",
+  "coca",
+];
+
+/* for (let index = 0; index < shoppingList.length; index++) {
+  console.log(shoppingList[index]);
+} */
+
+/* const newItemList = prompt("inserisci un prodotto che vuoi comprare"); */
+
+shoppingList.push(newItemList);
+
+console.log(shoppingList);
+
+for (let index = 0; index < 3; index++) {
+  const questionOne = prompt("Inserisci nome prodotto " + index);
+  const questionTwo = prompt("Inserisci prezzo prodotto " + index);
+}
